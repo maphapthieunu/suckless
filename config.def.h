313e05d384011b7f1f74b5e804fe10e7b09ba153
xnplz <phuclinhsama2009@gmail.com>
2022-04-11 16:02:33 +0700

    automated update

--- config.def.h
+++ config.def.h
@@ -63,7 +63,7 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2]			= "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[]		= { "rofi", "-show", "drun", "-font", "JetBrainsMono Nerd Font", NULL };
+static const char *dmenucmd[]		= { "rofi", "-show", "drun", "-font", "'JetBrainsMono Nerd Font'", NULL };
 static const char *termcmd[]		= { "kitty", NULL };
 static const char *fm[]			= { "thunar", NULL };
 static const char *upvol[]		= { "pactl", "set-sink-volume", "0", "+5%", NULL };
