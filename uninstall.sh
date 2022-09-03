49ce9a784fd1b4d879e3baef46a9d87feec71bf6
s1stine <npl@duck.com>
2022-09-03 10:40:11 +0700

    uninstall

---
+++ uninstall.sh
@@ -0,0 +1,10 @@
+clear
+cd dwm
+sudo make uninstall
+cd..
+cd dwmblocks
+sudo make uninstall
+
+clear
+echo Done!
+echo In case there's only my dwm build in your machine, please switch to another terminal (using Ctrl+Alt+Fx) and install a desktop environment.
